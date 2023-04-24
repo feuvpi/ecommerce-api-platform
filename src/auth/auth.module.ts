2c1bec9749d99e9c83d5441439f114199acbd07c
Fred Pinheiro <fredvpg@gmail.com>
2023-04-24 16:09:30 -0300

    restructuring project files, creating common folder

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { JwtService } from './jwt/jwt.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt/jwt.strategy';
+import { AuthService } from './auth.service';
 
 @Module({
-  providers: [JwtService],
+  imports: [JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: { expiresIn: '1h' },
+  })],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  exports: [AuthService]
 })
 export class AuthModule {}
+
+
