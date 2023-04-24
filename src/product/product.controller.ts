8e9a4f6970c6fbd7b38173fa566e8a732f9e4414
Fred Pinheiro <fredvpg@gmail.com>
2023-04-24 16:05:46 -0300

    setting up guard to protect admin routes

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -2,12 +2,13 @@ import { Controller, Get, Post, Put, Delete, Param, Body, UseGuards } from '@nes
 import { ProductService } from './product.service';
 import { Product } from './product.schema';
 import { AuthGuard } from '@nestjs/passport';
-import { JwtAuthGuard } from '../guard/auth.guard';
-import { JwtService } from '../auth/jwt/jwt.service';
+import { RolesGuard } from '../common/guards/role.guard';
+import { Role } from '../common/enums/roles.enums';
+import { Roles } from '../common/decorators/roles.decorator';
 
 @Controller('products')
 export class ProductController {
-  constructor(private readonly productService: ProductService, private readonly jwtService: JwtService) {}
+  constructor(private readonly productService: ProductService) {}
 
   @Get()
   @UseGuards(AuthGuard())
@@ -16,26 +17,28 @@ export class ProductController {
   }
 
   @Get(':id')
-  @UseGuards(AuthGuard())
+  @UseGuards(AuthGuard('jwt'))
   async findOne(@Param('id') id: string): Promise<Product> {
     return await this.productService.findById(id);
   }
 
   @Post()
-  @UseGuards(new JwtAuthGuard(this.jwtService, true))
+  @Roles(Role.Admin)
+  @UseGuards(RolesGuard)
   async create(@Body() product: Product): Promise<Product> {
     return await this.productService.create(product);
   }
 
   @Put(':id')
-  @UseGuards(new JwtAuthGuard(this.jwtService, true))
+  @Roles(Role.Admin)
+  @UseGuards(RolesGuard)
   async update(@Param('id') id: string, @Body() product: Product): Promise<Product> {
-    console.log(this.jwtService);
     return await this.productService.update(id, product);
   }
 
   @Delete(':id')
-  @UseGuards(new JwtAuthGuard(this.jwtService, true))
+  @Roles(Role.Admin)
+  @UseGuards(RolesGuard)
   async delete(@Param('id') id: string): Promise<Product> {
     return await this.productService.delete(id);
   }
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -4,10 +4,13 @@ import { Product, ProductSchema } from './product.schema';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtService } from 'src/auth/jwt/jwt.service';
+
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]), AuthModule],
-  providers: [ProductService],
+  imports: [MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]), AuthModule, JwtModule.register({ secret: process.env.JWT_SECRET_KEY })],
+  providers: [ProductService, JwtService],
   controllers: [ProductController],
 })
 export class ProductModule {}
